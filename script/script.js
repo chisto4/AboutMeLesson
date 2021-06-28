e17d911204846e9735c04fd224ac633e8c287611
chisto4 <chisto4@gmail.com>
2021-06-28 10:24:16 +0300

    Fixxed bugs, clear code, get up Button with jQuery

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
-$window.scroll(function() {
-  if($(window).scrollTop()>300){
-    $('#up').show()
+$(window).scroll(function(e) {
+  if($(window).scrollTop()>=700) {
+    $('div.up_Button').fadeIn(700);
+  } else {
+    $('div.up_Button').fadeOut(700);
   }
-})
+});
+
+$(document).ready(function() {
+  $(".menuToggle").click(function() {
+    $(this).toggleClass("active");
+    $('nav').slideToggle(300, function(){
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+      }
+    });
+  });
+});
